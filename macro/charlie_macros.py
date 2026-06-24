ff68d109719914e07522d7970cc43b7b12236f13
Michael Schneider <mschneid@mbi-berlin.de>
2026-06-24 19:21:29 +0200

    add config macros

---
+++ macro/charlie_macros.py
@@ -0,0 +1,85 @@
+import copy
+import os
+import pprint
+
+from sardana.macroserver.macro import Macro, Optional
+
+CHARLIE_ENV = "_CharlieConfiguration"
+
+
+def get_env(macro_obj, var=CHARLIE_ENV):
+    """Get a macroserver environment variable.
+
+    Defaults to _CharlieConfiguration.
+    """
+    try:
+        conf = copy.deepcopy(macro_obj.getEnv(var))
+    except Exception:
+        raise ValueError("Environment variable not found in environment!")
+    return conf
+
+
+def make_image_folder(macro_obj):
+    """Create new image subfolder based on next scanId in configured base folder."""
+    scanId = get_env(macro_obj, env="ScanID")
+    charlie_conf = get_env(macro_obj)
+    image_folder = os.path.join(charlie_conf["folder"], f"scan_{scanId:%06d}")
+    if not os.path.exists(image_folder):
+        os.mkdirs(image_folder)
+    return image_folder
+
+
+class charlie_conf(Macro):
+    """Show and alter current CHARLIE configuration."""
+
+    param_def = [
+        ["parameter", Optional, None, "Parameter to set or query."],
+        ["value", Optional, None, "Value to set (optional)."],
+    ]
+
+    def run(self, parameter=None, value=None):
+        try:
+            charlie_conf = get_env(self)
+        except Exception:
+            self.output("No CHARLIE configuration found. Creating default one.")
+            charlie_conf = {
+                "folder": get_env("ScanDir"),
+                "channel": "charlie",
+                "basename": "charlie",
+            }
+        if parameter is None:
+            # no parameter given -> print full config
+            self.print_config(charlie_conf)
+        elif value is None:
+            # parameter given, but no value -> output parameter value
+            self.print_config(charlie_conf, parameter)
+        else:
+            # both parameter and value given -> set parameter
+            # TODO: validate parameters!
+            charlie_conf[parameter] = value
+            self.setEnv(CHARLIE_ENV, charlie_conf)
+
+    def print_config(self, conf, parameter=None):
+        if parameter is None:
+            self.output(pprint.pformat(conf), width=16)
+        else:
+            self.output(f"{parameter} = {conf[parameter]}")
+
+
+class charlie_hook(Macro):
+    """Configure CHARLIE detector before scan."""
+
+    def run(self):
+        conf = get_env(self)
+        mg_active = self.getEnv("ActiveMntGrp")
+        mg = self.getMeasurementGroup(mg_active)
+
+        for channel in mg.getChannelLabels():
+            if channel != conf["channel"]:
+                continue
+
+            folder = make_image_folder(self)
+            file_pattern = os.path.join(folder, f"{conf['basename']}_")
+            self.set_meas_conf("ValueRefPattern", file_pattern, channel, mg)
+            self.set_meas_conf("ValueRefEnabled", True, channel, mg)
+
